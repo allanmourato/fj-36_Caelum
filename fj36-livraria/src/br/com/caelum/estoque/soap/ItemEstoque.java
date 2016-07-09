c73064d93ad51d663459853c3a3e67c3b097e4b2
allanmourato <allan.nunesmourato@gmail.com>
2016-07-09 15:02:23 -0300

    Finalizando

---
+++ fj36-livraria/src/br/com/caelum/estoque/soap/ItemEstoque.java
@@ -0,0 +1,87 @@
+
+package br.com.caelum.estoque.soap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for itemEstoque complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="itemEstoque">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="codigo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="quantidade" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "itemEstoque", propOrder = {
+    "codigo",
+    "quantidade"
+})
+public class ItemEstoque {
+
+    protected String codigo;
+    protected Integer quantidade;
+
+    /**
+     * Gets the value of the codigo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Sets the value of the codigo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCodigo(String value) {
+        this.codigo = value;
+    }
+
+    /**
+     * Gets the value of the quantidade property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * Sets the value of the quantidade property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setQuantidade(Integer value) {
+        this.quantidade = value;
+    }
+
+}
